4d42fa9151d4b6dad8da430c70b07a7ae3f97dd3
Vincent Brodin <vincent.brodin21@gmail.com>
2025-12-14 16:19:43 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Dotnet devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.dotnet-sdk_9
+          pkgs.dotnet-ef
+          pkgs.csharp-ls
+          pkgs.vscode-fhs
+          pkgs.sqlcmd
+          pkgs.nodejs_24
+        ];
+
+        shellHook = ''
+        '';
+      };
+    };
+}
+
